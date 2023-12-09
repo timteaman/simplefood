66557850f4e5a33f0a3c0bff9167eca7b2d8606f
timliketea <tim.like.tea@gmail.com>
2023-12-09 02:43:42 +0100

    add adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,78 @@
-const swiper = new Swiper(".review-slider", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
-  lazyLoading: true,
-  autoplay: {
-    delay: 3500,
-    disableOnIteraction: false
-  },
+function createSlider(selector, paginationEl, nextBtn, prevBtn) {
+  return new Swiper(selector, {
+    direction: "horizontal",
+    loop: true,
+    lazyLoading: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: paginationEl,
+      bulletClass: "slider-actions__dot",
+      bulletActiveClass: "slider-actions__dot--active",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: nextBtn,
+      prevEl: prevBtn,
+    },
+  });
+}
 
-  pagination: {
-    el: ".review-slider__dots",
-    bulletClass: 'slider-actions__dot',
-    bulletActiveClass: 'slider-actions__dot--active',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".review-slider__btn--next",
-    prevEl: ".review-slider__btn--prev",
-  },
+const reviewSlider = createSlider(
+  ".review-slider",
+  ".review-slider__dots",
+  ".review-slider__btn--next",
+  ".review-slider__btn--prev"
+);
+
+const restaurantSlider = createSlider(
+  ".restaurant-slider",
+  ".restaurant-slider__dots",
+  ".restaurant-slider__btn--next",
+  ".restaurant-slider__btn--prev"
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+  //DOMContentLoaded означает, когда наш документ будет готов к работе, тогда начнут работать наши скрипты
+
+  //Mobile Menu
+  const burger = document.querySelector(".burger"); //находим наш бургер по селектору класса
+
+  burger.addEventListener("click", () => {
+    //Добавляем событие "клик" на бургер
+
+    burger.classList.toggle("burger--active"); //при клике на бургер у нас будет либо добавлятся класс, либо удаляется.
+    //ВАЖНО! Мы уже работаем с данным классом, поэтому тут "." не ставим, иначе в атрибут class значение добавится с "." и работать не будет.
+  });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  //Mobile Menu
+  const burger = document.querySelector(".burger"); //наша кнопка
+  const mobileMenu = document.querySelector(".menu"); //мобильное меню
+  const bodyLock = document.querySelector("body"); //ищем как селектор ТЕГА
+
+  burger.addEventListener("click", () => {
+    mobileMenu.classList.toggle("menu--active"); //когда меню открыто
+    if (mobileMenu.classList.contains("menu--active")) {
+      //Проверяем, есть ли у меню активный класс
+      burger.classList.add("burger--active"); //Когда открыто, иконка становится крестиком
+      bodyLock.classList.add("lock"); //Блокируем скролл при открытом меню
+    } else {
+      //Когда нету активного класса у меню
+      burger.classList.remove("burger--active"); //Возвращает в исходное состояние
+      bodyLock.classList.remove("lock"); //Разрешаем скроллить
+    }
+  });
+});
+
+//Клик вне таргета
+document.addEventListener('click', function (e) {
+    if (e.target !== burger && e.target !== mobileMenu) {
+      burger.classList.remove('burger--active');
+      mobileMenu.classList.remove('menu--active');
+      bodyLock.classList.remove('lock');
+    }
+  });
