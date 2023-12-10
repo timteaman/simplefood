8b717b66326e1ee2de16643e23aa4ca4604dde6f
timliketea <tim.like.tea@gmail.com>
2023-12-10 19:22:40 +0100

    fix adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -34,45 +34,48 @@ const restaurantSlider = createSlider(
   ".restaurant-slider__btn--prev"
 );
 
-document.addEventListener("DOMContentLoaded", () => {
-  //DOMContentLoaded означает, когда наш документ будет готов к работе, тогда начнут работать наши скрипты
 
-  //Mobile Menu
-  const burger = document.querySelector(".burger"); //находим наш бургер по селектору класса
+//burger menu
 
-  burger.addEventListener("click", () => {
-    //Добавляем событие "клик" на бургер
+document.addEventListener('DOMContentLoaded', () => {
+	//DOMContentLoaded означает, когда наш документ будет готов к работе, тогда начнут работать наши скрипты
+  
+	//Mobile Menu
+  const burger = document.querySelector('.burger'); //находим наш бургер по селектору класса
+  
+  burger.addEventListener('click', () => {
+	//Добавляем событие "клик" на бургер
 
-    burger.classList.toggle("burger--active"); //при клике на бургер у нас будет либо добавлятся класс, либо удаляется.
-    //ВАЖНО! Мы уже работаем с данным классом, поэтому тут "." не ставим, иначе в атрибут class значение добавится с "." и работать не будет.
+  burger.classList.toggle('burger--active'); //при клике на бургер у нас будет либо добавлятся класс, либо удаляется.
+		//ВАЖНО! Мы уже работаем с данным классом, поэтому тут "." не ставим, иначе в атрибут class значение добавится с "." и работать не будет.
   });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  //Mobile Menu
-  const burger = document.querySelector(".burger"); //наша кнопка
-  const mobileMenu = document.querySelector(".menu"); //мобильное меню
-  const bodyLock = document.querySelector("body"); //ищем как селектор ТЕГА
+document.addEventListener('DOMContentLoaded', () => {
 
-  burger.addEventListener("click", () => {
-    mobileMenu.classList.toggle("menu--active"); //когда меню открыто
-    if (mobileMenu.classList.contains("menu--active")) {
-      //Проверяем, есть ли у меню активный класс
-      burger.classList.add("burger--active"); //Когда открыто, иконка становится крестиком
-      bodyLock.classList.add("lock"); //Блокируем скролл при открытом меню
-    } else {
-      //Когда нету активного класса у меню
-      burger.classList.remove("burger--active"); //Возвращает в исходное состояние
-      bodyLock.classList.remove("lock"); //Разрешаем скроллить
+  //Mobile Menu
+  const burger = document.querySelector('.burger'); //наша кнопка
+  const mobileMenu = document.querySelector('.burger__nav'); //мобильное меню
+  const bodyLock = document.querySelector('body'); //ищем как селектор ТЕГА
+
+  burger.addEventListener('click', () => {
+    mobileMenu.classList.toggle('burger__nav--active'); //когда меню открыто
+    if (mobileMenu.classList.contains('burger__nav--active'))  { //Проверяем, есть ли у меню активный класс
+      burger.classList.add('burger--active'); //Когда открыто, иконка становится крестиком
+      bodyLock.classList.add('lock'); //Блокируем скролл при открытом меню
+    }
+    else { //Когда нету активного класса у меню
+      burger.classList.remove('burger--active'); //Возвращает в исходное состояние
+      bodyLock.classList.remove('lock'); //Разрешаем скроллить
     }
   });
 });
 
 //Клик вне таргета
 document.addEventListener('click', function (e) {
-    if (e.target !== burger && e.target !== mobileMenu) {
-      burger.classList.remove('burger--active');
-      mobileMenu.classList.remove('menu--active');
-      bodyLock.classList.remove('lock');
-    }
-  });
+  if (e.target !== burger && e.target !== mobileMenu) {
+    burger.classList.remove('burger--active');
+    mobileMenu.classList.remove('burger__nav--active');
+    bodyLock.classList.remove('lock');
+  }
+});
